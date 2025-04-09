6dfc17ebc078afc5d1882ff1b4da33c74a560f05
Kaw77 <146209470+Kaw77@users.noreply.github.com>
2025-04-08 23:30:17 -0300

    Add files via upload

---
+++ compara valor int.py
@@ -0,0 +1,6 @@
+x = int(input('Digite um valor inteiro '))
+y = int(input('Digite um segundo valor inteiro '))
+if (x > y):
+    print('O primeiro valor é maior do que o segundo ')
+if (x < y):
+    print('O segundo valor é maior do que o primeiro ')   
