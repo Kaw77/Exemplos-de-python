48fc7ab52b398c06ab240301272a78232a498157
Kaw77 <146209470+Kaw77@users.noreply.github.com>
2025-04-09 17:09:21 -0300

    Add files via upload

---
+++ entrada invalida loop.py
@@ -0,0 +1,9 @@
+# Validando a entrada
+
+x = int(input('Digite um valor maior do que zero: '))
+
+while (x <= 0):
+
+  x = int(input('Digite um valor maior do que zero: '))
+
+print(f'Você digitou {x}. Encerrando o programa...')
