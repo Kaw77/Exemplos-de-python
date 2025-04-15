055add0257edf264854add76b80cbc65ca0371ef
Kaw77 <146209470+Kaw77@users.noreply.github.com>
2025-04-14 23:15:00 -0300

    Create imc.py

---
+++ imc.py
@@ -0,0 +1,35 @@
+pessoas = []
+
+imc = lambda peso, altura: peso / (altura * altura)
+
+while True:
+
+    nome = input('Nome: ')
+
+    altura = float(input('Altura (m): '))
+
+    peso = int(input('Peso (kg): '))
+
+    x = imc(peso, altura)
+
+    pessoas.append([nome, altura, peso, x])
+
+    res = input('Deseja fazer mais um cadastro? [S/N]')
+
+    if res in 'Nn':
+
+        break
+
+print('Cadastros: ', pessoas)
+
+print('Total de Cadastros: ', len(pessoas))
+
+maior = 0
+
+menor = 99
+
+for cadastro in pessoas:
+
+    if cadastro[3] > maior:
+
+        maior = cadastro[3]
